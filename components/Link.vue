79fb813865b4bd4dd6cf94fea73dc9c15592df71
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2022-06-08 13:24:07 -0700

    add Link component

---
+++ components/Link.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- Scroll To Element Using Hash -->
+  <a v-if="link.url.substr(0, 1) === '#'" :class="classes">{{ link.title }}</a>
+
+  <!-- Internal link -->
+  <nuxt-link
+    v-else-if="link.target != '_blank'"
+    :to="link.url"
+    :target="link.target"
+    :class="classes"
+    >{{ link.title }}</nuxt-link
+  >
+
+  <!-- External link -->
+  <a v-else :class="classes" :href="link.url" :target="link.target">{{
+    link.title
+  }}</a>
+</template>
+
+<script>
+export default {
+  props: {
+    link: Object,
+    classes: String,
+  },
+};
+</script>
